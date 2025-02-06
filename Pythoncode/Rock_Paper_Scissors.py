5e5eef9e24473402fbde63ff9b2463ebe1772961
JeslyP <jeslyprosper@gmail.com>
2025-02-06 16:43:32 -0500

    Create Rock_Paper_Scissors.py

---
+++ Pythoncode/Rock_Paper_Scissors.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+print("Welcomr to the Rock, Paper, Scissors Game")
+print ("1 is for Rock")
+print ("2 is for Paper")
+print ("3 is for Scissors")
+player1 = input("Enter your choice: rock, paper or scissors: ")
+
