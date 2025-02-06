6932f3a659b139c3f2a0c1d3a0181c5680cc94b2
JeslyP <jeslyprosper@gmail.com>
2025-02-06 17:11:09 -0500

    add random mod

--- Pythoncode/Rock_Paper_Scissors.py
+++ Pythoncode/Rock_Paper_Scissors.py
@@ -1,6 +1,4 @@
-
-
-
+import random   
 
 
 print("Welcome to the Rock, Paper, Scissors Game")
@@ -8,4 +6,9 @@ print ("1 is for Rock")
 print ("2 is for Paper")
 print ("3 is for Scissors")
 player1 = input("Enter your choice: rock, paper or scissors: ")
+computer = random.randint(1,3)
+
+1 = "rock"
+2 = "paper"
+3 = "scissors"
 
