dc8e5e0af5eb8cc91104d3195bc801d16cc94652
JeslyP <jeslyprosper@gmail.com>
2025-02-13 07:47:48 -0500

    final project done

--- Python/Pythoncode/Python Project/gif.py
+++ Python/Pythoncode/Python Project/gif.py
@@ -1,1 +1,9 @@
-import imageiop.v3 as iio
+import imageio.v3 as iio
+
+filenames = ['team-pic1.png', 'team-pic2.png']
+images = []
+for filename in filenames:
+    images.append(iio.imread(filename))
+iio.imwrite('team-pic.gif', images, duration=500, loop=0)
+
+
